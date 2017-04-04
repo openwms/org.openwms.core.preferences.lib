97f5a4311fb4c13cefc008b9dcfafc557441957d
Heiko Scherrer <scherrer@openwms.org>
2017-04-04 16:45:24 +0200

    takeover from main project org.openwms

--- src/main/java/org/openwms/core/configuration/ConfigurationApplicationContextConfiguration.java
+++ src/main/java/org/openwms/core/configuration/ConfigurationApplicationContextConfiguration.java
@@ -19,7 +19,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-
 package org.openwms.core.configuration;
 
 import org.springframework.context.annotation.Configuration;
---
+++ src/site/markdown/index.md
@@ -0,0 +1,2 @@
+# OpenWMS.org CORE Preferences
+
