2ca267f5d3a31bc618a502f895e9e60fb8cea835
Heiko Scherrer <hscherrer@interface21.io>
2021-06-11 09:54:39 +0200

    Disable CORS for WebFlux in development profile

--- src/main/java/org/openwms/core/configuration/app/PreferencesModuleConfiguration.java
+++ src/main/java/org/openwms/core/configuration/app/PreferencesModuleConfiguration.java
@@ -18,6 +18,8 @@ package org.openwms.core.configuration.app;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.ameba.annotation.EnableAspects;
 import org.ameba.app.BaseConfiguration;
+import org.ameba.app.SpringProfiles;
+import org.ameba.http.PermitAllCorsConfigurationSource;
 import org.ameba.mapping.BeanMapper;
 import org.ameba.mapping.DozerMapperImpl;
 import org.openwms.core.configuration.config.ModuleProperties;
@@ -30,12 +32,24 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+import javax.servlet.Filter;
 import javax.validation.Validator;
+
+import static java.util.Arrays.asList;
+import static org.ameba.Constants.HEADER_VALUE_X_IDENTITY;
+import static org.ameba.Constants.HEADER_VALUE_X_REQUESTID;
+import static org.ameba.Constants.HEADER_VALUE_X_TENANT;
 
 /**
  * A PreferencesModuleConfiguration.
@@ -48,6 +62,20 @@ import javax.validation.Validator;
 @EnableConfigurationProperties(ModuleProperties.class)
 @Import(BaseConfiguration.class)
 public class PreferencesModuleConfiguration {
+
+    @Profile(SpringProfiles.DEVELOPMENT_PROFILE)
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.setMaxAge(1800L);
+        corsConfig.setAllowedMethods(asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        corsConfig.setAllowedHeaders(asList("Content-Type", "X-REQUESTED-WITH", "Authorization", HEADER_VALUE_X_TENANT, HEADER_VALUE_X_REQUESTID, HEADER_VALUE_X_IDENTITY));
+        corsConfig.setExposedHeaders(asList("Location", HEADER_VALUE_X_TENANT, HEADER_VALUE_X_REQUESTID, HEADER_VALUE_X_IDENTITY));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(source);
+    }
 
     @Bean
     LocalServiceInitializer localServiceInitializer(ApplicationContext ctx) {
